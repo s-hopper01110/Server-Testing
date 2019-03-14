168e23063244d78bb1a8efe3b494e9d4fecac436
Sheila <s.hopper01110@gmail.com>
2019-03-14 19:20:33 -0400

    api for POST working - working on DELETE

--- api/server.js
+++ api/server.js
@@ -1,19 +1,48 @@
+//server-side testing 
+
 const express = require('express');
 
-// const hobbits = require('../hobbits/hobbitsModel.js');
+const doggos = require('../dogs/dogsModel.js');
 
 const server = express();
 
 server.use(express.json());
 
-// server.get('/', async (req, res) => {
-//   res.status(200).json({ api: 'up' });
-// });
+server.get('/', (req, res) => {
+  res.status(200).json({ message: 'working' });
+});
 
-// server.get('/hobbits', async (req, res) => {
-//   const rows = await hobbits.getAll();
+// server.get('/api/dogs', async (req, res) => {
+//   const rows = await dogs.getAll();
 
 //   res.status(200).json(rows);
 // });
+
+server.post('/api/dogs', (req,res) => {
+    const name = req.body.name
+    
+    if(name) {
+        doggos.insert(name)
+        res.status(201).json({ message: ' The dog has been created.' })
+    } else {
+        res.status(400).json({ message: ' Error adding dog to database' })
+    }
+})
+
+// // server.delete('/:id', ( req, res ) => {
+//     const { id } =req.params;
+//     db
+//     .remove(id)
+//     .then(posts => {
+//         if( posts ){
+//             res.status(204).end();
+//         }else{
+//             res.status(404).json({ success: false,  message:'The dog with the specified ID does not exist.' })
+//         }
+//     })
+//     .catch(err => {
+//         res.status(500).json({ error: 'The dog could not be removed' })
+//     })
+// })
 
 module.exports = server;
